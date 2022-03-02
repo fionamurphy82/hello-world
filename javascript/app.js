0d7cddf1f10d6a7c8e5988f61fcf0ff1c8570fab
Fiona <50021353@belfastmet.ac.uk>
2022-03-02 14:49:47 +0000

    fiona's work

---
+++ javascript/app.js
@@ -0,0 +1,9 @@
+function changeTheme(){
+
+    let colours = ["red", "green", "blue"];
+
+    for(i = 0; i < colours.length; i++){
+        //console.log(colours[i]);
+        document.body.style.backgroundColour = colours[i];
+        }
+}
